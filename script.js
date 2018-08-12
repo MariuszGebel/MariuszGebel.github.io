41bfef3efa28e1ac7027e377d3c7f1c55b56fc69
MariuszGebel <gebel@wp.pl>
2018-08-12 22:56:04 +0200

    Trello correction

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://boiling-inlet-42695.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://boiling-inlet-42695.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
